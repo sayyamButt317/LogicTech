593a2fb662d080b4f5381abdc0dfb2dd0a5befbe
Sayyam Butt <sayyambutt317@gmail.com>
2024-02-06 00:03:39 +0500

    updated

--- src/components/About/chooseus.jsx
+++ src/components/About/chooseus.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 function chooseus() {
   return (
-    <div>chooseus</div>
+  <div className='w-full h-auto md:max-md'>
+ 
+    <h1 className="font-serif text-black text-xl font-bold mb-4 md:w-1/2 md:px-6 py-6">
+            Transforming Lives Through Technology
+          </h1>
+    
+  </div>
   )
 }
 
