6cdfcf80c8a6b0ac2ac1e6328aa2eb462d2567d2
Sayyam Butt <sayyambutt317@gmail.com>
2024-02-12 19:04:16 +0500

    structure update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { useEffect } from "react";
 import Aos from "aos";
 import "./App.css";
-import Header from "./components/Utils/Header";
-import Footer from "./components/Utils/Footer";
-import Home from "./components/Home/Home";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "../src/pages/Home";
 function App() {
   useEffect(() => {
     Aos.init();
