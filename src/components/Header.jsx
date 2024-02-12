feee80d11fc771dcf43ca2516e1536a8e4d24368
IT <sayyambutt317@gmail.com>
2024-02-12 22:49:24 +0500

    folder structure

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
-
+import LOGO from "../components/Logo"
+import Chaticon from "../components/chaticon";
 const Header = () => {
   const headerRef = useRef(null);
 
@@ -25,16 +26,8 @@ const Header = () => {
   return (
     <header ref={headerRef} className="w-full h-20 flex items-center ">
       <div className="container flex items-center justify-between mx-auto px-4">
-        {/* Logo */}
-        <div className="flex items-center gap-2">
-          <span className="w-10 h-10 bg-indigo-500 text-white text-lg font-semibold rounded-full flex items-center justify-center">
-            LS
-          </span>
-          <div>
-            <h2 className="text-base md:text-xl font-bold">LogicTech Studio</h2>
-          </div>
-        </div>
-
+        
+      <LOGO/>
         {/* Menu */}
         <nav className="hidden md:flex items-center gap-8">
           <Link to="/" className="text-gray-600 hover:text-gray-900 transition duration-300">Home</Link>
@@ -44,17 +37,7 @@ const Header = () => {
         </nav>
 
         {/* Chat Icon */}
-        <div className="flex items-center gap-4">
-          <button
-            className="flex items-center gap-2 text-smallTextColor font-[600] border border-solid border-smallTextColor py-2 px-4 
-              rounded-[8px] max-h-[40px] hover:bg-smallTextColor hover:text-white hover:font-[500] ease-in duration-300"
-          >
-            <i className="ri-send-plane-line"></i>Lets' Talk
-          </button>
-          <span className="text-2xl text-smallTextColor md:hidden cursor-pointer">
-            <i className="ri-menu-line"></i>
-          </span>
-        </div>
+       <Chaticon/>
       </div>
     </header>
   );
